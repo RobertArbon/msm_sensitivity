5bb5b0dbcf8bdaff39c2fa3a3e841cf0db36b21c
rob arbon <robert.arbon@bristol.ac.uk>
2021-05-20 17:05:26 +0100

    removed multiprocessing cdoe

--- msmsense/bootstrap_cmatrices.py
+++ msmsense/bootstrap_cmatrices.py
@@ -141,20 +141,22 @@ def bootstrap_count_matrices(config: Tuple[str, Dict[str, List[Union[str, int]]]
     ftrajs = get_feature_trajs(traj_top_paths, hp_dict)
 
     n_workers = min(n_cores, bs_samples)
-    pool = Pool(n_workers)
+    # pool = Pool(n_workers)
     logging.info(f"Bootstrapping hyper-parameter index value {hp_idx}")
     logging.info(f'Launching {bs_samples} jobs on {n_workers} cores')
 
-    results = []
+    # results = []
     for i in range(bs_samples):
-        write_output = partial(write_matrices, sample_ix=i, out_dir=bs_dir)
-        results.append(pool.apply_async(func=do_bootstrap, args=(hp_dict, ftrajs, seed, lags), callback=write_output))
+        results = do_bootstrap(hp_dict, ftrajs, seed, lags)
+        write_matrices(results, out_dir=bs_dir, sample_ix=i)
+        # write_output = partial(write_matrices, sample_ix=i, out_dir=bs_dir)
+        # results.append(pool.apply_async(func=do_bootstrap, args=(hp_dict, ftrajs, seed, lags), callback=write_output))
 
-    for r in results:
-        r.get()
+    # for r in results:
+    #     r.get()
 
-    pool.close()
-    pool.join()
+    # pool.close()
+    # pool.join()
     logging.info(f'Finished boostrap hp_ix: {hp_idx}')
 
 
